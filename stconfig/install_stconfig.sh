e89394b00ff3be44ef30b926d219985916471d53
Jens Drenhaus <jens.drenhaus@9elements.com>
2019-11-07 15:07:45 +0100

    bugfix
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- stconfig/install_stconfig.sh
+++ stconfig/install_stconfig.sh
@@ -12,10 +12,12 @@ echo "[UROOT tooling] go install"
 GOPATH=$HOME/go go install $HOME/go/src/github.com/u-root/u-root/tools/stconfig
 echo "[UROOT tooling] install example files "
 cd $ABS
-echo "$BASE"
+if [ -d $BASE/../configs/example ]; then
+    rmdir -v --ignore-fail-on-non-empty $BASE/../configs/example
+fi
 mkdir -p $BASE/../configs/example/kernels
 mkdir -p $BASE/../configs/example/initrds
 mkdir -p $BASE/../configs/example/signing
-cp -r -v $BASE/../testitems/kernels $BASE/../configs/example/kernels
+cp -r -v $BASE/../testitems/kernels/* $BASE/../configs/example/kernels
 cp -r -v $BASE/../testitems/signing/* $BASE/../configs/example/signing
 cp -v $BASE/../testitems/manifest.json $BASE/../configs/example/manifest.json
