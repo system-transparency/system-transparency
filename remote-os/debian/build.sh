c842c1629c9cc3cde2aed612c14a5ee07a9a9499
Alexander Couzens <lynxis@fe80.eu>
2019-11-15 19:26:12 +0100

    remote-os/debian: check for docker environment and use --chroot
    To support docker, debos has to use the --chroot flags as docker does not support
    systemd-nspawn
    
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>

--- remote-os/debian/build.sh
+++ remote-os/debian/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # build a debian reproducible with debos
 # MIT
@@ -37,12 +37,24 @@ document_build_info() {
 }
 
 build_debian_image() {
+	dargs=()
+	if [ -e /.dockerenv ] ; then
+		if ! debos --help 2>&1 | grep -q -- --chroot ; then
+			echo "Your debos does not support --chroot !"
+			echo "Please use a newer debos version or the debos from https://github.com/system-transparency/debos"
+			exit 1
+		fi
+
+		dargs+=("--chroot")
+	fi
+
 	debos \
 		"--environ-var=SOURCE_DATE_EPOCH:$SOURCE_DATE_EPOCH" \
 		"--environ-var=LC_ALL:$LC_ALL" \
 		"--environ-var=LANG:$LANG" \
 		"--environ-var=TZ:$TZ" \
 		--artifactdir="$TOP/out/" \
+		"${dargs[@]}" \
 		./debos.yaml
 }
 
