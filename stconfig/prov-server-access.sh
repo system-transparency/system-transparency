031d96ccd6f036b83efcffb5fc2b5965dcaaaeed
Jens Drenhaus <jens.drenhaus@9elements.com>
2020-03-27 16:00:35 +0100

    add provisioning server config file for tooling
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

---
+++ stconfig/prov-server-access.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# The script upoad_bootball.sh uses this data during uplaod.
+# Upload is done via scp, so make sure ssh key are setup right on the server.
+
+# prov_server is the URL of the provisioning server.
+prov_server=""
+
+# prov_server_user is the username at the provisioning server.
+prov_server_user=""
+
+# prov_server_path is the web root of the provisioning server.
+prov_server_path=""
--- stconfig/upload_bootball.sh
+++ stconfig/upload_bootball.sh
@@ -5,9 +5,20 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
+# Set magic variables for current file & dir
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+root="$(cd "${dir}/../" && pwd)"
+
 failed="\e[1;5;31mfailed\e[0m"
-server="stboot.9esec.dev"
-server_path="/home/provisioner/www"
+
+config="${dir}/prov-server-access.sh"
+
+source ${config} || { echo -e "$failed : ${config} not found"; exit 1; }
+[ -z "$prov_server" ] && { echo -e "upload $failed : prov_server not set in $(realpath --relative-to=${root} ${config})"; exit 1; }
+[ -z "$prov_server_user" ] && { echo -e "upload $failed : prov_server_user not set in $(realpath --relative-to=${root} ${config})"; exit 1; }
+[ -z "$prov_server_path" ] && { echo -e " upload $failed : prov_server_path not set in $(realpath --relative-to=${root} ${config})"; exit 1; }
+
+
 
 bootball=""
 if [[ $# -eq 0 ]] ; then
@@ -19,6 +30,6 @@ else
 fi
 
 echo "[INFO]: upload ${bootball} to ${server_path} at ${server}"
-scp "$bootball" "provisioner@$server:${server_path}/$(basename "${bootball}")" || { echo -e "upload via scp $failed"; exit 1; }
+scp "$bootball" "${user}@${server}:${server_path}/$(basename "${bootball}")" || { echo -e "upload via scp $failed"; exit 1; }
 echo "[INFO]: successfully uploaded bootball"
 
