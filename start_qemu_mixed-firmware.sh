b6503db2799cfb804c28eba4e089b1d9158d3f02
Julian Laubstein <contact@julianlaubstein.de>
2020-04-07 17:10:50 +0200

    Adjusted initramfs creation; Updated ssh keys

--- start_qemu_mixed-firmware.sh
+++ start_qemu_mixed-firmware.sh
@@ -15,15 +15,16 @@ root="$dir"
 
 image="${root}/deploy/mixed-firmware/Syslinux_Linuxboot.img"
 
-qemu-system-x86_64 -drive if=virtio,file=${image},format=raw \
--object rng-random,filename=/dev/urandom,id=rng0 \
--device e1000,netdev=n1 \
--netdev user,id=n1,hostfwd=tcp:127.0.0.1:23-:2222,net=192.168.1.0/24,host=192.168.1.1 \
--rtc base=localtime \
--m 8192M \
--device virtio-rng-pci,rng=rng0 \
--serial stdio  \
-#-monitor /dev/null  \
-#-nographic \
-#-append earlyprintk=ttyS0,115200\ console=ttyS0 \
-
+qemu-system-x86_64
+  -drive if=virtio,file=${image},format=raw
+	-cpu max \
+  -s \
+  -m 1024m \
+  -machine q35  \
+  -nographic \
+  -object rng-random,filename=/dev/urandom,id=rng0 \
+  -device virtio-rng-pci,rng=rng0 \
+  -device e1000,netdev=n1 \
+  -netdev user,id=n1,hostfwd=tcp:127.0.0.1:23-:2222,net=192.168.1.0/24,host=192.168.1.1 \
+  -serial stdio  \
+  -append earlyprintk=ttyS0,115200\ console=ttyS0
