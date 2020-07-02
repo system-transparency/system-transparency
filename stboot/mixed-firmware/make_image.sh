ef451bef0041c628ee8a43c9117871637c8d84d5
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-07-02 16:53:10 +0200

    stboot/mixed-firmware/make_image: Small fixes
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- stboot/mixed-firmware/make_image.sh
+++ stboot/mixed-firmware/make_image.sh
@@ -26,15 +26,15 @@ mnt=$(mktemp -d -t stmnt-XXXXXXXX)
 
 if [ -f "${img}" ]; then
     while true; do
-       echo "Current image file:"
-       ls -l "$(realpath --relative-to="${root}" "${img}")"
-       read -rp "Rebuild image? (y/n)" yn
-       case $yn in
-          [Yy]* ) echo "[INFO]: backup existing image to $(realpath --relative-to="${root}" "${img_backup}")"; mv "${img}" "${img_backup}"; break;;
-          [Nn]* ) exit;;
-          * ) echo "Please answer yes or no.";;
-       esac
-    done
+        echo "Current image file:"
+        ls -l "$(realpath --relative-to=${root} ${img})"
+        read -rp "Rebuild image? (y/n)" yn
+        case $yn in
+            [Yy]* ) echo "[INFO]: backup existing image to $(realpath --relative-to=${root} ${img_backup})"; mv "${img}" "${img_backup}"; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+   done
 fi
 
 echo "[INFO]: check for Linuxboot kernel"
@@ -47,9 +47,8 @@ echo "[INFO]: check for Syslinux configuration"
 bash "${dir}/make_syslinux_config.sh"
 
 
-
 if [ -d "${src}" ]; then
-   echo "[INFO]: Using cached Syslinux in $(realpath --relative-to="${root}" "${src}")"
+   echo "[INFO]: Using cached Syslinux in $(realpath --relative-to=${root} ${src})"
 else
    echo "[INFO]: Downloading Syslinux Bootloader"
    wget "${syslinux_src}/${syslinux_tar}" -P "${src}"
@@ -140,5 +139,4 @@ echo "[INFO]: Image layout:"
 parted "${img}" print
 
 echo ""
-echo "[INFO]: $(realpath --relative-to="${root}" "${img}") created."
-
+echo "[INFO]: $(realpath --relative-to=${root} ${img}) created."
