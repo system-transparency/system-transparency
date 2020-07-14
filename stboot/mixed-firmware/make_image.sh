f1c0b2b7f464b7169f23f364acca09fbe05d73da
Patrick Rudolph <36034716+PatrickRudolph@users.noreply.github.com>
2020-07-14 09:34:19 +0200

    stboot/*/make_image: Use -s when calling parted (#35)
    Don't ask the user for confirmation, just do it.
    
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- stboot/mixed-firmware/make_image.sh
+++ stboot/mixed-firmware/make_image.sh
@@ -128,7 +128,7 @@ rm "${img}".ext4
 echo "[INFO]: Adding partitions to harddisk image:"
 
 # apply partitioning
-parted --align optimal "${img}" mklabel gpt mkpart "STBOOT" fat32 "$((offset_vfat * 512))B" "$((offset_vfat * 512 + size_vfat))B" mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B" set 1 boot on set 1 legacy_boot on
+parted -s --align optimal "${img}" mklabel gpt mkpart "STBOOT" fat32 "$((offset_vfat * 512))B" "$((offset_vfat * 512 + size_vfat))B" mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B" set 1 boot on set 1 legacy_boot on
 
 echo ""
 echo "[INFO]: Installing MBR"
@@ -136,7 +136,7 @@ dd bs=440 count=1 conv=notrunc if="${src}/${syslinux_dir}/bios/mbr/gptmbr.bin" o
 
 echo ""
 echo "[INFO]: Image layout:"
-parted "${img}" print
+parted -s "${img}" print
 
 echo ""
 echo "[INFO]: $(realpath --relative-to=${root} ${img}) created."
--- stboot/uefi-firmware/create_image.sh
+++ stboot/uefi-firmware/create_image.sh
@@ -78,11 +78,11 @@ rm "${img}".ext4
 echo "[INFO]: Adding partitions to harddisk image:"
 
 # apply partitioning
-parted --align optimal "${img}" mklabel gpt mkpart "STBOOT" fat32 "$((offset_vfat * 512))B" "$((offset_vfat * 512 + size_vfat))B" mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B" set 1 boot on set 1 legacy_boot on
+parted -s --align optimal "${img}" mklabel gpt mkpart "STBOOT" fat32 "$((offset_vfat * 512))B" "$((offset_vfat * 512 + size_vfat))B" mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B" set 1 boot on set 1 legacy_boot on
 
 echo ""
 echo "[INFO]: Image layout:"
-parted "${img}" print
+parted -s "${img}" print
 
 echo ""
 echo "[INFO]: $(realpath --relative-to=${root} ${img}) created."
