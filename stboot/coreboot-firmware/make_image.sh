486b969151fa61089d03f3bc0a2d5e32888f885d
Jens Drenhaus <jens.drenhaus@9elements.com>
2020-08-10 16:30:24 +0200

    add script to create stdata partition fore coreboot-firmware scenario
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

---
+++ stboot/coreboot-firmware/make_image.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Set magic variables for current file & dir
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+root="$(cd "${dir}/../../" && pwd)"
+
+img="${dir}/stdata.img"
+img_backup="${img}.backup"
+
+if [ -f "${img}" ]; then
+    while true; do
+        echo "Current image file:"
+        ls -l "$(realpath --relative-to="${root}" "${img}")"
+        read -rp "Rebuild image? (y/n)" yn
+        case $yn in
+            [Yy]* ) echo "[INFO]: backup existing image to $(realpath --relative-to="${root}" "${img_backup}")"; mv "${img}" "${img_backup}"; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+   done
+fi
+
+echo "[INFO]: Creating EXT4 filesystems for STDATA partition:"
+size_ext4=$((767*(1<<20)))
+alignment=1048576
+
+if [ -f "${img}".ext4 ]; then rm "${img}".ext4; fi
+mkfs.ext4 -L "STDATA" "${img}".ext4 $((size_ext4 >> 10))
+
+echo "[INFO]: Copying data files to image"
+ls -l "${root}/stboot/data/."
+
+e2mkdir "${img}".ext4:/etc
+e2mkdir "${img}".ext4:/stboot
+e2mkdir "${img}".ext4:/stboot/etc
+e2mkdir "${img}".ext4:/stboot/bootballs
+e2mkdir "${img}".ext4:/stboot/bootballs/new
+e2mkdir "${img}".ext4:/stboot/bootballs/invalid
+e2mkdir "${img}".ext4:/stboot/bootballs/known_good
+
+for i in "${root}/stboot/data"/*; do
+  [ -e "$i" ] || continue
+  e2cp "$i" "${img}".ext4:/stboot/etc
+done
+
+e2ls "${img}".ext4:/stboot/etc/
+
+echo "[INFO]: Copying bootballs to image (for LocalStorage bootmode)"
+ls -l "${root}/bootballs/."
+for i in "${root}/bootballs"/*; do
+  [ -e "$i" ] || continue
+  e2cp "$i" "${img}".ext4:/stboot/bootballs/new
+done
+
+echo "[INFO]: Constructing disk image from generated filesystems:"
+
+offset_ext4=$(( alignment/512 ))
+
+# insert the filesystem to a new file at offset 1MB
+dd if="${img}".ext4 of="${img}" conv=notrunc obs=512 status=none seek=${offset_ext4}
+
+# extend the file by 1MB
+truncate -s "+${alignment}" "${img}"
+
+# Cleanup
+rm "${img}".ext4
+
+echo "[INFO]: Adding partition to disk image:"
+
+# apply partitioning
+parted -s --align optimal "${img}" mklabel gpt mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B"
+
+echo ""
+echo "[INFO]: Image layout:"
+parted -s "${img}" print
+
+echo ""
+echo "[INFO]: $(realpath --relative-to="${root}" "${img}") created."
+
+echo ""
+echo "[INFO]: Creation of coreboot-rom not automated yet."
+echo "[INFO]: Plese follow the steps in stboot/coreboot-firmware/README.md"
