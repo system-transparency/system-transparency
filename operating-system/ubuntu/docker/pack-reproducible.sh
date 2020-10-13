1f89c0c816b8cb7014bd28206f023ef80694bd63
Christopher Meis <30509262+ChriMarMe@users.noreply.github.com>
2020-10-13 15:43:46 +0200

    combine ubuntu18 and ubuntu20 builds
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- operating-system/ubuntu/docker/pack-reproducible.sh
+++ operating-system/ubuntu/docker/pack-reproducible.sh
@@ -14,6 +14,6 @@ tar c "--mtime=@$SOURCE_DATE_EPOCH" --numeric-owner -C "$ROOTDIR" . | bzip2 -c -
 cd "$ROOTDIR" || exit 1
 
 # move kernel out of the image
-mv "$ROOTDIR"/boot/vmlinuz-4.15.0-20-generic "$ARTIFACTDIR/"
+mv "$ROOTDIR"/boot/vmlinuz-* "$ARTIFACTDIR/"
 find . | while read -r line ; do touch -hcd "@$SOURCE_DATE_EPOCH" "$line" ; done
 find . -mindepth 1 -printf '%P\0' | sort -z | cpio --reproducible -0 -o -H newc | gzip -9 -n > "$ARTIFACTDIR/$1.cpio.gz"
