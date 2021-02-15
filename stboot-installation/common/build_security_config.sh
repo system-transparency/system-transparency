c414e2a128689f21d3c20994c68c01612b2eada0
Jens Drenhaus <jens.drenhaus@9elements.com>
2021-02-15 12:54:44 +0100

    update security_configuration.json
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- stboot-installation/common/build_security_config.sh
+++ stboot-installation/common/build_security_config.sh
@@ -15,8 +15,11 @@ source ${root}/run.config
 out="${root}/out/stboot-installation"
 name="security_configuration.json"
 security_config="${out}/${name}"
+
+version=1
 num_signatures=${ST_NUM_SIGNATURES}
 boot_mode=${ST_BOOT_MODE}
+use_ospkg_cache=${ST_USE_PKG_CACHE}
 
 if [ ! -d "${out}" ]; then mkdir -p "${out}"; fi
 
@@ -25,8 +28,10 @@ echo "[INFO]: Creating $(realpath --relative-to="${root}" "${security_config}")"
 
 cat >"${security_config}" <<EOL
 {
+  "version":${version},
   "minimal_signatures_match": ${num_signatures},
-  "boot_mode": "${boot_mode}"
+  "boot_mode": "${boot_mode}",
+  "use_ospkg_cache": ${use_ospkg_cache}
 }
 EOL
 
