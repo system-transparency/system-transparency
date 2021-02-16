c5b23d7ba52490d3abf9c3dfb3566226c9bf3076
Marcello Sylvester Bauer <sylv@sylv.io>
2021-02-16 10:19:52 +0100

    debos: fix default recipes
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- operating-system/debos/scripts/pack_reproducibly.sh
+++ operating-system/debos/scripts/pack_reproducibly.sh
@@ -7,21 +7,16 @@ set -o nounset
 kernel_out="$ARTIFACTDIR/$1.vmlinuz"
 archive_out="$ARTIFACTDIR/$1.cpio.gz"
 
-if [ -z "$SOURCE_DATE_EPOCH" ] ; then
-  echo "Environment SOURCE_DATE_EPOCH is not set!" >&2
-  exit 1
-fi
-
-echo "set date of files to SOURCE_DATE_EPOCH"
+echo "set date of files to SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH"
 touch -hcd "@$SOURCE_DATE_EPOCH" "${ROOTDIR}/boot/vmlinuz-"*
 find ${ROOTDIR} | while read -r line ; do touch -hcd "@$SOURCE_DATE_EPOCH" "$line" ; done
 
 echo "moving kernel to ${kernel_out}"
-cp "${ROOTDIR}/boot/vmlinuz-"* "${kernel_out}"
-rm -f "${ROOTDIR}/boot/vmlinuz-"*
+cp "root/boot/vmlinuz-"* "${kernel_out}"
+rm -f "root/boot/vmlinuz-"*
 
 echo "creating ${archive_out} ..."
-cd ${ROOTDIR}
+cd ${ROOTDIR} || exit 1
 find . -print0 | cpio --reproducible -0 -o -H newc | gzip -9 -n > "${archive_out}"
 
 trap - EXIT
