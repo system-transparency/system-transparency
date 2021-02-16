5534c27d4efb03f06ee9081c60fc6867e99e21a2
Marcello Sylvester Bauer <sylv@sylv.io>
2021-02-16 08:12:35 +0100

    fix pack_reproducibly.sh
    * fix absolute path of file
    * extend debos recipes
    
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

---
+++ operating-system/debos/debos-overlays/root/EXAMPLE.md
@@ -0,0 +1,1 @@
+Overlay example file
--- operating-system/debos/scripts/pack_reproducibly.sh
+++ operating-system/debos/scripts/pack_reproducibly.sh
@@ -21,6 +21,7 @@ cp "${ROOTDIR}/boot/vmlinuz-"* "${kernel_out}"
 rm -f "${ROOTDIR}/boot/vmlinuz-"*
 
 echo "creating ${archive_out} ..."
-find ${ROOTDIR} -print0 | cpio --reproducible -0 -o -H newc | gzip -9 -n > "${archive_out}"
+cd ${ROOTDIR}
+find . -print0 | cpio --reproducible -0 -o -H newc | gzip -9 -n > "${archive_out}"
 
 trap - EXIT
