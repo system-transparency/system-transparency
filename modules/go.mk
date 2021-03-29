6ca930758868de0c58a1c3774e113875068f47cc
Marcello Sylvester Bauer <sylv@sylv.io>
2021-03-29 11:56:01 +0200

    Makefile: export all variable
    define special target `.EXPORT_ALL_VARIABLES` to export all
    make variables to child processes
    
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- modules/go.mk
+++ modules/go.mk
@@ -1,23 +1,23 @@
 ## debos
-debos_bin := $(gopath)/bin/debos
+debos_bin := $(GOPATH)/bin/debos
 # do not edit debos_packages
 debos_package := github.com/go-debos/debos
 # change debos_repo to use a fork of debos
 debos_repo := github.com/system-transparency/debos
-debos_src := $(gopath)/src/$(debos_package)
+debos_src := $(GOPATH)/src/$(debos_package)
 # uses the remote defined in debos_repo
 debos_branch := system-transparency
 ## u-root
-u-root_bin := $(gopath)/bin/u-root
-stmanager_bin := $(gopath)/bin/stmanager
+u-root_bin := $(GOPATH)/bin/u-root
+stmanager_bin := $(GOPATH)/bin/stmanager
 u-root_package := github.com/u-root/u-root
-u-root_src := $(gopath)/src/$(u-root_package)
+u-root_src := $(GOPATH)/src/$(u-root_package)
 u-root_default_branch := stboot
 ## ACM grebber
-sinit-acm-grebber_bin := $(gopath)/bin/sinit-acm-grebber
+sinit-acm-grebber_bin := $(GOPATH)/bin/sinit-acm-grebber
 sinit-acm-grebber_package := github.com/system-transparency/sinit-acm-grebber
 ## cpu command for debugging
-cpu_bin := $(gopath)/bin/cpu
+cpu_bin := $(GOPATH)/bin/cpu
 cpud_bin := $(cpu_bin)d
 cpu_package := github.com/u-root/cpu/cmds/cpu
 cpud_package := $(cpu_package)d
@@ -38,7 +38,7 @@ define go_update
 	else \
 	echo [Go] Install $(1); \
 	fi;
-	GOPATH=$(gopath) go build $4 -o $(2).temp $(3)
+	go build $4 -o $(2).temp $(3)
 	if [ -x $(2) ] && diff $(2) $(2).temp >/dev/null; then \
 	echo [Go] $(1) already up-to-date; \
 	fi
@@ -65,7 +65,7 @@ debos_checkout := $(debos_src)/.git/HEAD
 
 $(debos_get):
 	@echo [Go] Get $(debos_package)
-	GOPATH=$(gopath) go get -d -u $(debos_package)/...
+	go get -d -u $(debos_package)/...
 $(debos_remote): $(debos_get)
 	if ! git -C $(debos_src) remote show system-transparency >/dev/null 2>&1; then \
 	  echo [Go] Add system-transparecy remote $(debos_repo); \
@@ -104,7 +104,7 @@ u-root_checkout := $(u-root_src)/.git/HEAD
 
 $(u-root_get): $(go_check)
 	@echo [Go] Get $(u-root_package)
-	GOPATH=$(gopath) go get -d -u $(u-root_package)
+	go get -d -u $(u-root_package)
 	git -C $(u-root_src) checkout --quiet $(u-root_default_branch)
 u-root_fetch $(u-root_fetch): $(u-root_get)
 	@echo [Go] Fetch branch $(u-root_branch)
@@ -137,7 +137,7 @@ $(u-root_bin) $(stmanager_bin): $(u-root_checkout)
 
 cpu $(cpu_bin) $(cpud_bin)$(GROUP_TARGET):
 	@echo [Go] Get $(cpu_package)
-	GO111MODULE=off GOPATH=$(gopath) go get -d -u $(cpu_package)
+	go get -d -u $(cpu_package)
 	$(call go_update,cpu,$(cpu_bin),$(cpu_package))
 	$(call go_update,cpud,$(cpud_bin),$(cpud_package))
 
@@ -145,7 +145,7 @@ cpu $(cpu_bin) $(cpud_bin)$(GROUP_TARGET):
 
 sinit-acm-grebber $(sinit-acm-grebber_bin):
 	@echo [Go] Get $(sinit-acm-grebber_package)
-	GO111MODULE=off GOPATH=$(gopath) go get -d -u $(sinit-acm-grebber_package)
+	go get -d -u $(sinit-acm-grebber_package)
 	$(call go_update,sinit-acm-grebber,$(sinit-acm-grebber_bin),$(sinit-acm-grebber_package))
 
 .PHONY: go-tools debos debos_checkout debos_get u-root stmanager u-root_checkout u-root_get cpu sinit-acm-grebber
