system-transparency/system-transparency

commit 88149e54498b52dd8b102a2fa661ba2725473630

Makefile: export all variable
define special target `.EXPORT_ALL_VARIABLES` to export all
make variables to child processes

Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

Makefile
@@ -9,6 +9,9 @@ stboot-installation := stboot-installation
 newest-ospkg := .newest-ospkg.zip
 initramfs := $(out)/stboot-installation/initramfs-linuxboot.cpio.gz
 
+# export all variables to child processes
+.EXPORT_ALL_VARIABLES:
+
 # reproducible builds
 LANG:=C
 LC_ALL:=C
@@ -20,22 +23,22 @@ SHELL := /usr/bin/env bash -euo pipefail -c
 # setup development environment if ST_DEVELOP=1
 ifeq ($(patsubst "%",%,$(ST_DEVELOP)),1)
 	# use local GOPATH
-	gopath := $(shell source <(go env) && echo $$GOPATH)
+	GOPATH := $(shell source <(go env) && echo $$GOPATH)
 endif
 
-# use custom gopath
+# use custom GOPATH
 ifneq ($(ST_GOPATH),)
 # have to be an absolute path
 ifeq ($(shell [[ $(ST_GOPATH) = /* ]] && echo y),y)
-gopath := $(ST_GOPATH)
+GOPATH := $(ST_GOPATH)
 else
 $(error ST_GOPATH have to be an absolute path!)
 endif
 endif
 
-# default gopath
-ifeq ($(gopath),)
-gopath := $(CURDIR)/cache/go
+# default GOPATH
+ifeq ($(GOPATH),)
+GOPATH := $(CURDIR)/cache/go
 endif
 
 # Make is silent per default, but 'make V=1' will show all compiler calls.
@@ -245,7 +248,7 @@ keygen: keygen-sign keygen-cpu
 
 keygen-sign: $(stmanager_bin)
 	@echo [stboot] Generate example signing keys
-	GOPATH=$(gopath) $(scripts)/make_signing_keys.sh $(OUTREDIRECT)
+	$(scripts)/make_signing_keys.sh $(OUTREDIRECT)
 	@echo [stboot] Done example signing keys
 
 keygen-cpu: $(call GROUP,$(CPU_SSH_KEYS))
@@ -257,7 +260,7 @@ $(call GROUP,$(CPU_SSH_KEYS))$(GROUP_TARGET):
 
 sign: $(DOTCONFIG) $(ROOT_CERT) $(KEYS_CERTS) $(call GROUP,$(OS_KERNEL) $(OS_INITRAMFS)) $(stmanager_bin) $(tboot) acm
 	@echo [stboot] Sign OS package
-	GOPATH=$(gopath) $(scripts)/create_and_sign_os_package.sh $(OUTREDIRECT)
+	$(scripts)/create_and_sign_os_package.sh $(OUTREDIRECT)
 	@echo [stboot] Done sign OS package
 
 upload: $(newest-ospkg)

modules/go.mk
@@ -1,23 +1,23 @@
 ## debos
-debos_bin := $(gopath)/bin/debos
+debos_bin := $(GOPATH)/bin/debos
 # do not edit debos_packages
 debos_package := github.com/go-debos/debos
 # change debos_repo to use a fork of debos
 debos_repo := github.com/system-transparency/debos
-debos_src := $(gopath)/src/$(debos_package)
+debos_src := $(GOPATH)/src/$(debos_package)
 # uses the remote defined in debos_repo
 debos_branch := system-transparency
 ## u-root
-u-root_bin := $(gopath)/bin/u-root
-stmanager_bin := $(gopath)/bin/stmanager
+u-root_bin := $(GOPATH)/bin/u-root
+stmanager_bin := $(GOPATH)/bin/stmanager
 u-root_package := github.com/u-root/u-root
-u-root_src := $(gopath)/src/$(u-root_package)
+u-root_src := $(GOPATH)/src/$(u-root_package)
 u-root_default_branch := stboot
 ## ACM grebber
-sinit-acm-grebber_bin := $(gopath)/bin/sinit-acm-grebber
+sinit-acm-grebber_bin := $(GOPATH)/bin/sinit-acm-grebber
 sinit-acm-grebber_package := github.com/system-transparency/sinit-acm-grebber
 ## cpu command for debugging
-cpu_bin := $(gopath)/bin/cpu
+cpu_bin := $(GOPATH)/bin/cpu
 cpud_bin := $(cpu_bin)d
 cpu_package := github.com/u-root/cpu/cmds/cpu
 cpud_package := $(cpu_package)d
@@ -38,7 +38,7 @@ define go_update
 	else \
 	echo [Go] Install $(1); \
 	fi;
-	GOPATH=$(gopath) go build $4 -o $(2).temp $(3)
+	go build $4 -o $(2).temp $(3)
 	if [ -x $(2) ] && diff $(2) $(2).temp >/dev/null; then \
 	echo [Go] $(1) already up-to-date; \
 	fi
@@ -65,7 +65,7 @@ debos_checkout := $(debos_src)/.git/HEAD
 
 $(debos_get):
 	@echo [Go] Get $(debos_package)
-	GOPATH=$(gopath) go get -d -u $(debos_package)/...
+	go get -d -u $(debos_package)/...
 $(debos_remote): $(debos_get)
 	if ! git -C $(debos_src) remote show system-transparency >/dev/null 2>&1; then \
 	  echo [Go] Add system-transparecy remote $(debos_repo); \
@@ -104,7 +104,7 @@ u-root_checkout := $(u-root_src)/.git/HEAD
 
 $(u-root_get): $(go_check)
 	@echo [Go] Get $(u-root_package)
-	GOPATH=$(gopath) go get -d -u $(u-root_package)
+	go get -d -u $(u-root_package)
 	git -C $(u-root_src) checkout --quiet $(u-root_default_branch)
 u-root_fetch $(u-root_fetch): $(u-root_get)
 	@echo [Go] Fetch branch $(u-root_branch)
@@ -137,7 +137,7 @@ $(u-root_bin) $(stmanager_bin): $(u-root_checkout)
 
 cpu $(cpu_bin) $(cpud_bin)$(GROUP_TARGET):
 	@echo [Go] Get $(cpu_package)
-	GO111MODULE=off GOPATH=$(gopath) go get -d -u $(cpu_package)
+	go get -d -u $(cpu_package)
 	$(call go_update,cpu,$(cpu_bin),$(cpu_package))
 	$(call go_update,cpud,$(cpud_bin),$(cpud_package))
 
@@ -145,7 +145,7 @@ cpu $(cpu_bin) $(cpud_bin)$(GROUP_TARGET):
 
 sinit-acm-grebber $(sinit-acm-grebber_bin):
 	@echo [Go] Get $(sinit-acm-grebber_package)
-	GO111MODULE=off GOPATH=$(gopath) go get -d -u $(sinit-acm-grebber_package)
+	go get -d -u $(sinit-acm-grebber_package)
 	$(call go_update,sinit-acm-grebber,$(sinit-acm-grebber_bin),$(sinit-acm-grebber_package))
 
 .PHONY: go-tools debos debos_checkout debos_get u-root stmanager u-root_checkout u-root_get cpu sinit-acm-grebber

operating-system/Makefile.inc
@@ -11,11 +11,11 @@ include $(op)/debos/Makefile.inc
 
 tboot_code := $(tboot-cache)/code/.unpack
 
-TBOOT_CFLAGS="-Wno-error=address-of-packed-member"
+_TBOOT_CFLAGS="-Wno-error=address-of-packed-member"
 gcc_version := $(shell gcc -dumpversion | cut -d . -f 1)
 
 ifeq ($(shell [[ "$(gcc_version)" -ge "9" ]] && echo $$?),0)
-tboot_env := CFLAGS=$(TBOOT_CFLAGS) TBOOT_CFLAGS=$(TBOOT_CFLAGS)
+tboot_env := CFLAGS=$(_TBOOT_CFLAGS) TBOOT_CFLAGS=$(_TBOOT_CFLAGS)
 endif
 
 $(tboot_code):

stboot-installation/common/Makefile.inc
@@ -29,7 +29,7 @@ else
 $(initramfs): % : %.config $(u-root_bin) $(security_config) $(ROOT_CERT)
 endif
 	@echo "[stboot] Build initramfs"
-	GOPATH=$(gopath) $(common)/build_initramfs.sh $(OUTREDIRECT)
+	$(common)/build_initramfs.sh $(OUTREDIRECT)
 	@echo "[stboot] Done initramfs"
 
 $(eval $(call CONFIG_DEP,$(host_config),ST_PROVISIONING_SERVER_URL|ST_NETWORK_MODE|ST_HOST_IP|ST_HOST_GATEWAY|ST_HOST_DNS))

commit ccb486837d068c5d1caf20bf6b2958f6edd3dea7

build: Re-add GO111MODULE
The GO111MODULE environment variable was removed, which broke the
build. Let us re-add it.

Fixes: 88149e54498b ("Makefile: export all variable")
Signed-off-by: Björn Töpel <bjorn@mullvad.net>

Makefile
@@ -41,6 +41,7 @@ endif
 ifeq ($(GOPATH),)
 GOPATH := $(CURDIR)/cache/go
 endif
+GO111MODULE := off
 
 ## logging color
 ifneq ($(TERM),)