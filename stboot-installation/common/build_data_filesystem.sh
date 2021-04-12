4eeea98dfd05bb1c60b2ad60b30fdf9016b2b81d
Marcello Sylvester Bauer <sylv@sylv.io>
2021-04-12 13:04:02 +0200

    replace ST_DATA_PARTITION_SZIZE with ST_DATA_PARTITION_EXTRA_SPACE
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- stboot-installation/common/build_data_filesystem.sh
+++ stboot-installation/common/build_data_filesystem.sh
@@ -12,24 +12,33 @@ root="$(cd "${dir}/../../" && pwd)"
 # import global configuration
 source "${DOTCONFIG:-.config}"
 
+toBytes() {
+ echo $1 | echo $((`sed 's/.*/\L\0/;s/t/Xg/;s/g/Xm/;s/m/Xk/;s/k/X/;s/b//;s/X/ *1024/g'`))
+}
+
 out="${root}/out/stboot-installation"
 name="data_partition.ext4"
 fs="${out}/${name}"
 local_boot_order_file_name="boot_order"
 local_boot_order_file="${root}/out/os-packages/${local_boot_order_file_name}"
 os_pkg_dir="${root}/out/os-packages"
-size_ext4="${ST_DATA_PARTITION_SZIZE}"
 
 if [ ! -d "${os_pkg_dir}" ]; then mkdir -p "${os_pkg_dir}"; fi
 
-# if empty set to size of os_pkg_dir + 100MB
-if [ -z "${size_ext4}" ]; then size_ext4=$(( $(du -b "${os_pkg_dir}" | cut -f1) +(100*(1<<20)) )); fi
+size_data_used=$(( $(du -b "${os_pkg_dir}" | cut -f1) ))
+size_data_extra=$(toBytes "${ST_DATA_PARTITION_EXTRA_SPACE:-0}")
+
+#size_data=$(( 20000000 + ${size_data_used} + ${size_data_extra} ))
+size_data=$(( ${size_data_used} + ${size_data_extra} ))
+inode_size=256
+inode_ratio=16384
+size_ext4=$(echo "scale=6;(${size_data}*(1.10+(${inode_size}/${inode_ratio})))+1" | bc -l | xargs printf "%0.f")
 
 echo
 echo "[INFO]: Creating EXT4 filesystems for STDATA partition:"
 
 if [ -f "${fs}.tmp" ]; then rm "${fs}.tmp"; fi
-mkfs.ext4 -L "STDATA" "${fs}.tmp" $((size_ext4 >> 10))
+mkfs.ext4 -I "${inode_size}" -i "${inode_ratio}" -L "STDATA" "${fs}.tmp" $((${size_ext4} >> 10))
 
 e2mkdir "${fs}.tmp":/stboot
 e2mkdir "${fs}.tmp":/stboot/etc
