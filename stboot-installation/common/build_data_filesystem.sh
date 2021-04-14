22797db5a6caa6f69b436c4842351a35fd73934e
Marcello Sylvester Bauer <sylv@sylv.io>
2021-04-14 13:44:11 +0000

    STDATA extra space (#95)
    Create partitions (e.g. STDATA, STBOOT) and images using a temporary
    file. As soon as it is complete, move it to the actual location.
    
    * replace ST_DATA_PARTITION_SZIZE with ST_DATA_PARTITION_EXTRA_SPACE
    * fix u-root variant "full"
    
    Add u-root path analog to "minimal" and  "debug: variant.
    
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- stboot-installation/common/build_data_filesystem.sh
+++ stboot-installation/common/build_data_filesystem.sh
@@ -12,46 +12,57 @@ root="$(cd "${dir}/../../" && pwd)"
 # import global configuration
 source "${DOTCONFIG:-.config}"
 
+toBytes() {
+ echo $1 | echo $((`sed 's/.*/\L\0/;s/t/Xg/;s/g/Xm/;s/m/Xk/;s/k/X/;s/b//;s/X/ *1024/g'`))
+}
+
 out="${root}/out/stboot-installation"
 name="data_partition.ext4"
 fs="${out}/${name}"
 local_boot_order_file_name="boot_order"
 local_boot_order_file="${root}/out/os-packages/${local_boot_order_file_name}"
 os_pkg_dir="${root}/out/os-packages"
-size_ext4="${ST_DATA_PARTITION_SZIZE}"
 
 if [ ! -d "${os_pkg_dir}" ]; then mkdir -p "${os_pkg_dir}"; fi
 
-# if empty set to size of os_pkg_dir + 100MB
-if [ -z "${size_ext4}" ]; then size_ext4=$(( $(du -b "${os_pkg_dir}" | cut -f1) +(100*(1<<20)) )); fi
+size_data_used=$(( $(du -b "${os_pkg_dir}" | cut -f1) ))
+size_data_extra=$(toBytes "${ST_DATA_PARTITION_EXTRA_SPACE:-0}")
+
+#size_data=$(( 20000000 + ${size_data_used} + ${size_data_extra} ))
+size_data=$(( ${size_data_used} + ${size_data_extra} ))
+inode_size=256
+inode_ratio=16384
+size_ext4=$(echo "scale=6;(${size_data}*(1.10+(${inode_size}/${inode_ratio})))+1" | bc -l | xargs printf "%0.f")
 
 echo
 echo "[INFO]: Creating EXT4 filesystems for STDATA partition:"
 
-if [ -f "${fs}" ]; then rm "${fs}"; fi
-mkfs.ext4 -L "STDATA" "${fs}" $((size_ext4 >> 10))
+if [ -f "${fs}.tmp" ]; then rm "${fs}.tmp"; fi
+mkfs.ext4 -I "${inode_size}" -i "${inode_ratio}" -L "STDATA" "${fs}.tmp" $((${size_ext4} >> 10))
 
-e2mkdir "${fs}":/stboot
-e2mkdir "${fs}":/stboot/etc
-e2mkdir "${fs}":/stboot/os_pkgs
-e2mkdir "${fs}":/stboot/os_pkgs/local
-e2mkdir "${fs}":/stboot/os_pkgs/cache
+e2mkdir "${fs}.tmp":/stboot
+e2mkdir "${fs}.tmp":/stboot/etc
+e2mkdir "${fs}.tmp":/stboot/os_pkgs
+e2mkdir "${fs}.tmp":/stboot/os_pkgs/local
+e2mkdir "${fs}.tmp":/stboot/os_pkgs/cache
 
 echo
 echo "[INFO]: Writing UNIX timestamp"
 timestamp_file="${out}/system_time_fix"
 date +%s > "${timestamp_file}"
 cat "${timestamp_file}"
-e2cp "${timestamp_file}" "${fs}":/stboot/etc
+e2cp "${timestamp_file}" "${fs}.tmp":/stboot/etc
 rm "${timestamp_file}"
 
 echo
 echo "[INFO]: Copying OS packages to image (for local boot method)"
 ls -l "${root}/out/os-packages/."
-e2cp "${local_boot_order_file}" "${fs}":/stboot/os_pkgs/local
+e2cp "${local_boot_order_file}" "${fs}.tmp":/stboot/os_pkgs/local
 for i in "${os_pkg_dir}"/*; do
   [ -e "$i" ] || continue
-  e2cp "$i" "${fs}":/stboot/os_pkgs/local
+  e2cp "$i" "${fs}.tmp":/stboot/os_pkgs/local
 done
+
+mv ${fs}{.tmp,}
 
 trap - EXIT
--- stboot-installation/mbr-bootloader/build_image.sh
+++ stboot-installation/mbr-bootloader/build_image.sh
@@ -12,19 +12,12 @@ root="$(cd "${dir}/../../" && pwd)"
 out="${root}/out/stboot-installation/mbr-bootloader"
 name="stboot_mbr_installation.img"
 img="${out}/${name}"
-img_backup="${img}.backup"
 syslinux_src="https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/"
 syslinux_tar="syslinux-6.03.tar.xz"
 syslinux_dir="syslinux-6.03"
 syslinux_cache="${root}/cache/syslinux/"
 boot_filesystem="${out}/boot_partition.vfat"
 data_filesystem="${out}/../data_partition.ext4"
-
-if [ -f "${img}" ]; then
-    echo
-    echo "[INFO]: backup existing image to $(realpath --relative-to="${root}" "${img_backup}")"
-    mv "${img}" "${img_backup}"
-fi
 
 if [ ! -d "${out}" ]; then mkdir -p "${out}"; fi
 
@@ -34,28 +27,28 @@ echo "[INFO]: Using : $(realpath --relative-to="${root}" "${boot_filesystem}")"
 echo "[INFO]: Using : $(realpath --relative-to="${root}" "${data_filesystem}")"
 
 alignment=1048576
-#size_vfat=$((12*(1<<20)))
 size_vfat=$(du -b "${boot_filesystem}" | cut -f1)
-#size_ext4=$((767*(1<<20)))
 size_ext4=$(du -b "${data_filesystem}" | cut -f1)
 offset_vfat=$(( alignment/512 ))
 offset_ext4=$(( (alignment + size_vfat + alignment)/512 ))
 
 # insert the filesystem to a new file at offset 1MB
-dd if="${boot_filesystem}" of="${img}" conv=notrunc obs=512 status=none seek=${offset_vfat}
-dd if="${data_filesystem}" of="${img}" conv=notrunc obs=512 status=none seek=${offset_ext4}
+dd if="${boot_filesystem}" of="${img}.tmp" conv=notrunc obs=512 status=none seek=${offset_vfat}
+dd if="${data_filesystem}" of="${img}.tmp" conv=notrunc obs=512 status=none seek=${offset_ext4}
 
 # extend the file by 1MB
-truncate -s "+${alignment}" "${img}"
+truncate -s "+${alignment}" "${img}.tmp"
 
 echo "[INFO]: Adding partitions to disk image:"
 
 # apply partitioning
-parted -s --align optimal "${img}" mklabel gpt mkpart "STBOOT" fat32 "$((offset_vfat * 512))B" "$((offset_vfat * 512 + size_vfat))B" mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B" set 1 boot on set 1 legacy_boot on
+parted -s --align optimal "${img}.tmp" mklabel gpt mkpart "STBOOT" fat32 "$((offset_vfat * 512))B" "$((offset_vfat * 512 + size_vfat))B" mkpart "STDATA" ext4 "$((offset_ext4 * 512))B" "$((offset_ext4 * 512 + size_ext4))B" set 1 boot on set 1 legacy_boot on
 
 echo ""
 echo "[INFO]: Installing MBR"
-dd bs=440 count=1 conv=notrunc if="${syslinux_cache}/${syslinux_dir}/bios/mbr/gptmbr.bin" of="${img}" status=none
+dd bs=440 count=1 conv=notrunc if="${syslinux_cache}/${syslinux_dir}/bios/mbr/gptmbr.bin" of="${img}.tmp" status=none
+
+mv ${img}{.tmp,}
 
 echo ""
 echo "[INFO]: Image layout:"
