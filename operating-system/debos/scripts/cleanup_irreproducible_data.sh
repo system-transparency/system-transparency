a7ab9cf82992989adac3a85837af7f5a75bb080d
Marcello Sylvester Bauer <sylv@sylv.io>
2021-04-23 13:40:30 +0200

    debos: Fix scripts
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- operating-system/debos/scripts/cleanup_irreproducible_data.sh
+++ operating-system/debos/scripts/cleanup_irreproducible_data.sh
@@ -2,7 +2,8 @@
 
 set -o errexit
 set -o nounset
-# set -o xtrace
+
+ROOTDIR=${ROOTDIR:-}
 
 # remove systemd machine id
 rm -f ${ROOTDIR}/etc/machine-id
@@ -20,7 +21,7 @@ rm -rf ${ROOTDIR}/var/lib/systemd/catalog/database
 find ${ROOTDIR}/var/log -type f | while read -r line ; do rm -f "$line" ; done
 
 # remove pycache
-find ${ROOTDIR} -type d -name __pycache__ | while read -r line ; do rm -rf "$line" ; done
+find ${ROOTDIR}/ -type d -name __pycache__ | while read -r line ; do rm -rf "$line" ; done
 
 # remove initrd as its not needed nor reproducible
 rm -rf ${ROOTDIR}/var/lib/initramfs-tools/*
--- operating-system/debos/scripts/pack_reproducibly.sh
+++ operating-system/debos/scripts/pack_reproducibly.sh
@@ -12,8 +12,8 @@ touch -hcd "@$SOURCE_DATE_EPOCH" "${ROOTDIR}/boot/vmlinuz-"*
 find ${ROOTDIR} | while read -r line ; do touch -hcd "@$SOURCE_DATE_EPOCH" "$line" ; done
 
 echo "moving kernel to ${kernel_out}"
-cp "root/boot/vmlinuz-"* "${kernel_out}"
-rm -f "root/boot/vmlinuz-"*
+cp ${ROOTDIR}/boot/vmlinuz-* "${kernel_out}"
+rm -f ${ROOTDIR}/boot/vmlinuz-*
 
 echo "creating ${archive_out} ..."
 cd ${ROOTDIR} || exit 1
