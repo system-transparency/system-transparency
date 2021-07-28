38fd6cbd928d9125bccda4588b733fd73c8e54f3
Marcello Sylvester Bauer <sylv@sylv.io>
2021-07-28 11:57:57 +0200

    Init task_config.sh
    Create Task helper script to handle config file depenencies.
    It scans the script for configuration files and creates a subconfig
    for the target. As soon as it changed, a rebuild is triggered via
    target status.
    
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

---
+++ .task_config.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -eu
+#set -x
+
+# config file location
+CONFIG_FILE='.config'
+# location to save target subconfigs
+CONFIG_DATA='.task/config'
+# config name pattern
+# XXX: define a naming convention
+CONFIG_PAT='ST_[A-Z_]*'
+
+if [[ "$#" -ne 2 ]]; then
+	echo "Usage ${0##*/} <name> <script>"
+	exit 1
+fi
+
+NAME="${1}"
+SCRIPT="${2}"
+
+# target subconfig file location
+SUBCONFIG="${CONFIG_DATA}/${NAME}"
+
+if [[ ! -f "${SUBCONFIG}" ]]; then
+	mkdir -p "${CONFIG_DATA}"
+	if ! touch "${SUBCONFIG}"; then
+		>&2 echo "Failed to create subconfig: \"${SUBCONFIG}\""
+		exit 1
+	fi
+fi
+
+# check if config file exist
+if [[ ! -f "${CONFIG_FILE}" ]]; then
+	>&2 echo "Config file \"${CONFIG_FILE}\" missing"
+	exit 1
+fi
+
+source "${CONFIG_FILE}"
+
+# check if script exist
+if [[ ! -f "${SCRIPT}" ]]; then
+	>&2 echo "Script \"${SCRIPT}\" not found"
+	exit 1
+fi
+
+# read all accuring configs into a sorted array
+read -a CONFIG_LIST <<< "$(grep -o "${CONFIG_PAT}" "${SCRIPT}" | sort | uniq | tr '\n' ' ')"
+
+# create temporary subconfig
+tmp_subconfig="$(mktemp)"
+trap "rm -f ${tmp_subconfig}" EXIT
+
+# parse relevant configs to temporary subconfig
+for config in "${CONFIG_LIST[@]}"; do
+	echo "${config}=\"${!config:-}\"" >> $tmp_subconfig
+done
+
+# check if subconfig changed
+if diff "${tmp_subconfig}" "${SUBCONFIG}"; then
+	echo "no change in subconfig"
+else
+	echo "subconfig changed"
+	cp "${tmp_subconfig}" "${SUBCONFIG}"
+	exit 1
+fi
