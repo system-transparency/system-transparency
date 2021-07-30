2b2d07e521d85201c6c4207146a4dd2d38e6bf45
Marcello Sylvester Bauer <sylv@sylv.io>
2021-07-30 11:02:41 +0200

    install-task.sh: current dir as default path
    Install task into the current path, if flag -b is not defined.
    
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- install-task.sh
+++ install-task.sh
@@ -9,7 +9,7 @@ usage() {
 $this: download go binaries for go-task/task
 
 Usage: $this [-b] bindir [-d] [tag]
-  -b sets bindir or installation directory, Defaults to ./bin
+  -b sets bindir or installation directory, Defaults to ./.
   -d turns on debug logging
    [tag] is a tag from
    https://github.com/go-task/task/releases
@@ -23,10 +23,10 @@ EOF
 }
 
 parse_args() {
-  #BINDIR is ./bin unless set be ENV
+  #BINDIR is ./. unless set be ENV
   # over-ridden by flag below
 
-  BINDIR=${BINDIR:-./bin}
+  BINDIR=${BINDIR:-.}
   while getopts "b:dh?x" arg; do
     case "$arg" in
       b) BINDIR="$OPTARG" ;;
