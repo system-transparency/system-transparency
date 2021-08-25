02585a8fafc063c117bfde3a10b3ed988f8cb67b
Marcello Sylvester Bauer <sylv@sylv.io>
2021-08-25 14:28:00 +0200

    env.sh: prevent executation
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- env.sh
+++ env.sh
@@ -1,15 +1,16 @@
+#!/bin/false "This script should be sourced in a shell, not executed directly"
+
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    >&2 echo "usage: source $0"
+    >&2 echo "please source this shell script!"
+    exit 1
+fi
+
 TASKBIN="${PWD}/bin"
 GOBIN="${PWD}/cache/go/bin"
 TASK="${TASKBIN}/task"
 
 [[ -x "${TASK}" ]] || scripts/install-task.sh -b "${TASKBIN}"
-
-if [[ $PATH != *${TASKBIN}* ]];
-then
-  export PATH=${LOCALBIN}:$PATH
-fi
-
-if [[ $PATH != *${GOBIN}* ]];
-then
-  export PATH=${GOBIN}:$PATH
-fi
+[[ $PATH != *${TASKBIN}* ]] || export PATH=${TASKBIN}:$PATH
+[[ $PATH != *${GOBIN}* ]] || export PATH=${GOBIN}:$PATH
