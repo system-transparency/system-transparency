c3bf99b996f8ffdf110d54a6b24bcc4f9fac333e
Marcello Sylvester Bauer <sylv@sylv.io>
2021-11-05 12:06:04 +0100

    workflow/testing: test EFI boot MBR installation (#124)
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- .github/workflows/scripts/test-qemu.sh
+++ .github/workflows/scripts/test-qemu.sh
@@ -21,6 +21,8 @@ case $TARGET in
 		;;
 esac
 
+BOOTLOADER=${2:-$TARGET}
+
 declare -a MATCH
 #Ubuntu Focal
 MATCH+=("Ubuntu 20.04 LTS ubuntu ttyS0")
@@ -40,14 +42,14 @@ trap cleanup 0
 # run qemu
 source st.config
 ST_LOCAL_OSPKG_DIR=$ST_LOCAL_OSPKG_DIR ST_BOOT_MODE=$ST_BOOT_MODE ./scripts/qemu_run.sh \
-    -b "${TARGET}" -i "out/${IMAGE}" </dev/null | tee /dev/stderr > "$LOG" &
+    -b "${BOOTLOADER}" -i "out/${IMAGE}" </dev/null | tee /dev/stderr > "$LOG" &
 
 i=0
 while [ "$i" -lt "$TIMEOUT" ]
 do
 	for m in "${MATCH[@]}"
 	do
-		if grep -q "$m" "$LOG"
+		if grep -q "$m" "$LOG" >/dev/null 2>&1
 		then
 			echo "loginshell reached. Boot successful"
 			exit 0
