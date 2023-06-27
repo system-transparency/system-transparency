db8dfb378c509d0abe2c886e18ef3e9aaf4718d4
Kai Michaelis <kai.michaelis@immu.ne>
2023-06-27 19:08:06 +0200

    Fix minor issues in build-stprov.sh
    * Break long lines
    * Add missing quotes around variables
    * Make it a bash script as it uses bash features

--- contrib/testing/build-stprov.sh
+++ contrib/testing/build-stprov.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 set -eu
 set -x
@@ -7,13 +7,28 @@ set -x
 task linux:kernel-prebuilt demo:initramfs-stprov demo:ospkg-stprov
 
 # => out/ospkgs/stprov.zip
-stmgr ospkg create -out 'out/ospkgs/stprov.zip' -label='Provisioning Tool' -kernel=out/artifacts/stboot.vmlinuz -initramfs=out/artifacts/stprov.cpio.gz -cmdline=''
+stmgr ospkg create \
+  -out 'out/ospkgs/stprov.zip' \
+  -label='Provisioning Tool' \
+  -kernel=out/artifacts/stboot.vmlinuz \
+  -initramfs=out/artifacts/stprov.cpio.gz \
+  -cmdline=''
 
 # => out/ospkgs/stprov.json
-for i in {1..2}; do cache/go/bin/stmgr ospkg sign -key=out/keys/example_keys/signing-key-$i.key -cert=out/keys/example_keys/signing-key-$i.cert -ospkg out/ospkgs/stprov.zip; done
+for i in {1..2}
+do
+  cache/go/bin/stmgr ospkg sign \
+    -key="out/keys/example_keys/signing-key-$i.key" \
+    -cert="out/keys/example_keys/signing-key-$i.cert" \
+    -ospkg out/ospkgs/stprov.zip
+done
 
 # => out/artifacts/stboot.cpio.gz
 task iso-provision
 
 # => out/stprov.iso
-stmgr uki create -format iso -out out/stprov.iso -kernel contrib/linuxboot.vmlinuz -initramfs out/artifacts/stprov.cpio.gz -cmdline=''
+stmgr uki create -format iso \
+  -out out/stprov.iso \
+  -kernel contrib/linuxboot.vmlinuz \
+  -initramfs out/artifacts/stprov.cpio.gz \
+  -cmdline=''
