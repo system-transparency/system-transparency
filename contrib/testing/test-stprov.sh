9b9f42d134c46a504f7b0f1a48fd96f039f02650
Kai Michaelis <kai.michaelis@immu.ne>
2023-06-27 19:09:22 +0200

    Add missing quotes in test-stprov.sh

--- contrib/testing/test-stprov.sh
+++ contrib/testing/test-stprov.sh
@@ -8,13 +8,13 @@ stboot=stboot-${n}.iso
 
 ./build-ospkg.sh
 ./build-stprov.sh
-scp out/stprov.iso tee.sigsum.org:/var/lib/sambashares/iso/${stprov}
+scp out/stprov.iso "tee.sigsum.org:/var/lib/sambashares/iso/${stprov}"
 
 echo "Mount ISO $stprov in BMC and restart server"
 read -r
 
 ./build-stboot.sh
-scp out/stboot.iso tee.sigsum.org:/var/lib/sambashares/iso/${stboot}
+scp out/stboot.iso "tee.sigsum.org:/var/lib/sambashares/iso/${stboot}"
 scp out/ospkgs/os-pkg-example-ubuntu20.* tee.sigsum.org:/var/www/netboot/
 
 echo "Mount ISO $stboot in BMC and restart server"
