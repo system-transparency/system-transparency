302152cd1e1cf2b2612867d95f358a5a2bfdbdbc
Kai Michaelis <kai.michaelis@immu.ne>
2023-06-27 18:58:38 +0200

    Fix minor issues in build-ospkg.sh
    * Make it a bash script, it uses bash features
    * Break long lines
    * Some quoting issues

--- contrib/testing/build-ospkg.sh
+++ contrib/testing/build-ospkg.sh
@@ -1,10 +1,22 @@
-#! /bin/sh
+#! /bin/bash
 
 set -eu
 set -x
 
 task demo:ospkg
 
-stmgr ospkg create -out 'out/ospkgs/os-pkg-example-ubuntu20.zip' -label='System Transparency Test OS' -kernel=cache/debos/ubuntu-focal-amd64.vmlinuz -initramfs=cache/debos/ubuntu-focal-amd64.cpio.gz -cmdline='rw rdinit=/lib/systemd/systemd' -url=http://192.168.67.2/os-pkg-example-ubuntu20.zip
+stmgr ospkg create \
+  -out 'out/ospkgs/os-pkg-example-ubuntu20.zip' \
+  -label='System Transparency Test OS' \
+  -kernel=cache/debos/ubuntu-focal-amd64.vmlinuz \
+  -initramfs=cache/debos/ubuntu-focal-amd64.cpio.gz \
+  -cmdline='rw rdinit=/lib/systemd/systemd' \
+  -url=http://192.168.67.2/os-pkg-example-ubuntu20.zip
 
-for i in {1..2}; do cache/go/bin/stmgr ospkg sign -key=out/keys/example_keys/signing-key-$i.key -cert=out/keys/example_keys/signing-key-$i.cert -ospkg out/ospkgs/os-pkg-example-ubuntu20.zip; done
+for i in {1..2}
+do
+  cache/go/bin/stmgr ospkg sign \
+    -key="out/keys/example_keys/signing-key-$i.key" \
+    -cert="out/keys/example_keys/signing-key-$i.cert" \
+    -ospkg out/ospkgs/os-pkg-example-ubuntu20.zip
+done
